76ed3182c7d31f0eff85ddd31f97a0cea9cb3040
Akshat Kedia <akshat.kedia@outlook.com>
2018-06-17 15:51:49 +0530

    update actionmailer smtp settings for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,4 +17,14 @@ Myflix::Application.configure do
   config.i18n.fallbacks = true
 
   config.active_support.deprecation = :notify
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              ENV['MAILGUN_SMTP_SERVER'],
+    port:                 ENV['MAILGUN_SMTP_PORT'],
+    domain:               'ls-myflix.herokuapp.com',
+    user_name:            ENV['MAILGUN_SMTP_LOGIN'],
+    password:             ENV['MAILGUN_SMTP_PASSWORD'],
+    authentication:       'plain',
+  }
 end
